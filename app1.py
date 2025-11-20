d4bf10dc87778db694438233a652a53b9fe459fc
KSUKRio <krio1008.uv@gmail.com>
2025-11-20 16:19:59 +0900

    Fix typo in camera input function name

--- app1.py
+++ app1.py
@@ -5,7 +5,7 @@ st.title("こんにちは、吉村ゼミ")
 name = st.text_input("名前を入力")
 st.write(name)
 
-camera = st.camera_imput("写真を撮影します！")
+camera = st.camera_input("写真を撮影します！")
 if camera:
   st.image(camera, caption="写真", use_column_width=True)
   

e55f6e110ba6db9a8518def7d0ebdc7c5235d17a
KSUKRio <krio1008.uv@gmail.com>
2025-11-20 16:18:37 +0900

    Implement camera input for photo capture
    Add camera input feature to capture and display images.

--- app1.py
+++ app1.py
@@ -3,5 +3,9 @@ import streamlit as st
 st.title("こんにちは、吉村ゼミ")
 
 name = st.text_input("名前を入力")
-
 st.write(name)
+
+camera = st.camera_imput("写真を撮影します！")
+if camera:
+  st.image(camera, caption="写真", use_column_width=True)
+  
